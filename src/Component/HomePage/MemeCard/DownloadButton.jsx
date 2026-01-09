e8f8a3ac063f34b4765e2297973301c5de6e1817
KaranBhavsar-1 <karanbhavsar0001@gmail.com>
2026-01-09 14:10:23 +0000

    Fix image paths for Vercel-5

--- src/Component/HomePage/MemeCard/DownloadButton.jsx
+++ src/Component/HomePage/MemeCard/DownloadButton.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import downloadIcon from "../../assets/images/DownloadButton.png"; // adjust path
+// import downloadIcon from "../../assets/images/DownloadButton.png"; // adjust path
+import downloadIcon from "../../../assets/images/DownloadButton.png";
 
 function DownloadButton() {
   const [animate, setAnimate] = useState(false);
